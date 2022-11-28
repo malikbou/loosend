c84cc42eb4dde3828536195d907c2fbfa0e8c0fd
Chirantan <45467698+ChirantanS@users.noreply.github.com>
2022-11-28 17:58:26 +0000

    Routes edit first - may fix tomorrow

--- app/models/toilet_feature.rb
+++ app/models/toilet_feature.rb
@@ -1,4 +1,7 @@
 class ToiletFeature < ApplicationRecord
   belongs_to :toilet
   belongs_to :feature
+
+  has_many :features
+  has_many :toilets, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :toilets, only: [:show] do
+    resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
