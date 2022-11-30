16cf1a72666d89c1201c968fa6447380e86368b4
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-30 01:39:43 +0000

    successfully added geocode to app

--- Gemfile
+++ Gemfile
@@ -80,3 +80,5 @@ end
 gem "simple_form", github: "heartcombo/simple_form"
 
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+
+gem "geocoder"
--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,4 +1,7 @@
 class Toilet < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :toilet_features
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
