05a6ea6dda82ce0b452758d50101f338fb68b09c
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-30 13:08:31 +0000

    created an index page and made homepage bare

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :toilets, only: [:show] do
+  resources :toilets, only: [:index, :show] do
     resources :reviews, only: [:index, :new, :create]
   end
   resources :reviews, only: [:show, :edit, :update, :destroy]
