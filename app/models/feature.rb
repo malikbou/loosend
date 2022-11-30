7417fb88b79a5f73e27ced2e16951eb8923d00a2
Chirantan <45467698+ChirantanS@users.noreply.github.com>
2022-11-30 13:15:45 +0000

    new update - updated as told

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,3 @@
 class Feature < ApplicationRecord
+  has_many :toilet_features
 end
--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -2,6 +2,9 @@ class Toilet < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :toilet_features
 
+  validates :name, :address, :opens_at, :closes_at, :fee, :toilet_code, :rating, presence: true
+  validates :fee, numericality: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
