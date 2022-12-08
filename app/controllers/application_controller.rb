48bba656e04e77951a21956ead90b1d2efa58b9f
Malik Bouaoudia <malik@bouaoudia.com>
2022-12-08 10:46:07 +0000

    new fix for naked domain redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :redirect_root_domain
+
+  private
+
+  def redirect_root_domain
+    return unless request.host === 'loosend.co'
+
+    redirect_to("#{request.protocol}www.loosend.co#{request.fullpath}", status: 301)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :reviews, only: [:show, :edit, :update, :destroy]
 
   # Naked Domain redirected to www
-  match "(*any)", to: redirect(subdomain: ""), via: :all, constraints: { subdomain: "www" }
+  # match "(*any)", to: redirect(subdomain: ""), via: :all, constraints: { subdomain: "www" }
 end
